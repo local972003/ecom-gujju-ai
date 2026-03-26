15ca4e168092f89e6b2de8ffd18d6ec1076403e6
local972003 <sanghaniromil@gmail.com>
2026-03-26 14:55:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+async function uploadImage() {
+  const file = document.getElementById("image").files[0];
+  const loader = document.getElementById("loaderOverlay");
+  const output = document.getElementById("output");
+
+  if (!file) {
+    alert("Please upload image");
+    return;
+  }
+
+  output.innerHTML = "";
+  loader.classList.remove("hidden");
+
+  const formData = new FormData();
+  formData.append("image", file);
+
+  try {
+    const res = await fetch("http://localhost:3000/analyze-image", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await res.json();
+
+    loader.classList.add("hidden");
+
+    output.innerHTML = `
+      <div class="bg-white/10 p-4 rounded-xl border border-white/20">
+        <b class="text-blue-400">📌 Title</b><br>${data.title}
+      </div>
+
+      <div class="bg-white/10 p-4 rounded-xl border border-white/20">
+        <b class="text-blue-400">📝 Description</b><br>${data.description}
+      </div>
+
+      <div class="bg-white/10 p-4 rounded-xl border border-white/20">
+        <b class="text-blue-400">🔍 Keywords</b><br>${data.keywords}
+      </div>
+    `;
+
+  } catch (err) {
+    loader.classList.add("hidden");
+    output.innerHTML = "❌ Error generating result";
+  }
+}
